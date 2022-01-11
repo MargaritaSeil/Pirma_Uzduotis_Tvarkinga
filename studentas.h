5ad205ae4bcc9b7bd6a29e09de44f9cfe8622984
MargaritaSeil <90559062+MargaritaSeil@users.noreply.github.com>
2022-01-11 16:38:55 +0200

    Add files via upload

---
+++ studentas.h
@@ -0,0 +1,15 @@
+#ifndef STUDENTAS_H     
+#define STUDENTAS_H     
+
+#include <vector>
+#include <string>
+
+struct studentas{
+    std::string vardas, pavarde;
+    std::vector <float> paz;
+    double med;
+    float egz;
+    float galutinis_paz;
+};
+
+#endif                  
